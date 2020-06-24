b4936ecea582cf5d9a91f8996d1b197750f7aa6e
Julia Mitropolskiy <jkovaleva@gmail.com>
2020-06-24 11:48:42 +0200

    ajout d'offre

--- src/app/services/offre.service.ts
+++ src/app/services/offre.service.ts
@@ -1,5 +1,7 @@
+import { Offre } from './../models/Offre.model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +10,14 @@ export class OffreService {
 
   constructor(private http: HttpClient) { }
 
-  getAllOffres() {
+  getAllOffres(): Observable<any> {
     return this.http.get('http://localhost:8081/offres');
+  }
+
+  createOffre(titre: string, text: string, userId: number, rubriqueId: string) {
+    const body = { 'titre': titre, 'text': text, 'userId': userId, 'rubriqueId': rubriqueId };
+
+    return this.http.post('http://localhost:8081/offres/new', body);
   }
 
 }
